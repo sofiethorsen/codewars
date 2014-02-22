f10c64b480d5366c109c319d14d785df378410f7
Johan Brodin <johan.brodin@gmail.com>
2014-02-22 13:36:07 +0100

    ignore!

---
+++ challenge3/server/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+
+
+var app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+
+
+app.use("/js", express.static(__dirname + '/client/js'));
+app.use("/css", express.static(__dirname + '/client/css'));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/client/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});
