ca0461b673ae8ab67b5851a5fd786d4375b3d06d
Alexander Heldt <me@alexanderheldt.se>
2014-02-22 20:26:18 +0100

    Fix special color

--- challenge7/server/client/js/pong.js
+++ challenge7/server/client/js/pong.js
@@ -50,7 +50,7 @@ function refresh(data) {
             if (block.visible) {
                 var elem = $('<div class="block"/>');
                 elem.css({top: block.y, left: block.x, width: block.width, height: block.height});
-                if block.special {
+                if(block.special) {
                     elem.css({color: orange})
                 }
                 $("#game").append(elem);
