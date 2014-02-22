b1fc271494e149f7c06521207604c1d5dbd72089
Alexander Blom <me@alexanderblom.se>
2014-02-22 14:01:12 +0100

    Add very good server paddle

--- challenge3/server/game/index.js
+++ challenge3/server/game/index.js
@@ -2,6 +2,13 @@ console.log("game start");
 
 var PADDLE_WIDTH = 20;
 var PADDLE_HEIGHT = 50;
+var WIDTH = 1000;
+var HEIGHT = 500;
+
+var PADDLE_SPEED = 10;
+
+var paddle_left = Paddle("left");
+var paddle_right = Paddle("right");
 
 function Player(options) {
 
@@ -9,14 +16,29 @@ function Player(options) {
 
 function Paddle(side) {
     var top = 0;
+    var left = side == "left" ? 0 : WIDTH - PADDLE_WIDTH;
     var direction = 0;
 
     return {
+        height: function() {
+            return PADDLE_HEIGHT;
+        },
+        setDirection: function(direction) {
+            if (direction == "up") {
+                direction = -1;
+            } else if (direction == "down") {
+                direction = 1;
+            } else {
+                direction = 0;
+            }
+        },
         update: function() {
-
+            top += direction * PADDLE_SPEED;
+            top = Math.min(500 - this.height(), top);
         }
-    }
-}
+
+    };
+};
 
 var state;
 var player = null;
