2a990cb62acc66fb60ab5a83f219cd27d44f95b8
Alexander Blom <me@alexanderblom.se>
2014-02-22 10:30:11 +0100

    Add Paddle function

--- challenge1/js/pong.js
+++ challenge1/js/pong.js
@@ -20,5 +20,22 @@ player = function(options) {
     init();
 }
 
+Paddle = function(node) {
+    return {
+        move: function(offset) {
+            var o = node.position();
+            o.top += offset;
+            o.top = Math.max(0, o.top);
+            o.top = Math.min(500 - node.height(), o.top);
+            node.css(o);
+        }
+    };
+}
+
+$(document).ready(function() {
+    var left = Paddle($("#left"));
+    left.move(2000);
+});
+
 player1 = player({name: 'klas', keyUp: 38, keyDown: 40}); // up/down arrows
 player2 = player({name: 'gustaf', keyUp: 65, keyDown: 90}); // a - up, z - down
