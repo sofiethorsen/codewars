15a3fc7d7460e8bee6a269161a2e5b25a8006827
Alexander Heldt <me@alexanderheldt.se>
2014-02-22 10:43:49 +0100

    Add score to player

--- challenge1/js/pong.js
+++ challenge1/js/pong.js
@@ -1,7 +1,8 @@
-player = function(options) {
+Player = function(options) {
     var NAME = options.name;
     var KEY_UP = options.keyUp;
     var KEY_DOWN = options.keyDown;
+    var score = 0;
 
     init = function() {
         controls();
@@ -17,7 +18,20 @@ player = function(options) {
         });
     };
 
+    _score = function() {
+        return score;
+    }
+
+    _updateScore = function() {
+        return score += 1;
+    };
+
     init();
+
+    return {
+        score: _score,
+        updateScore: _updateScore
+    };
 }
 
 Paddle = function(node) {
@@ -37,5 +51,5 @@ $(document).ready(function() {
     left.move(2000);
 });
 
-player1 = player({name: 'klas', keyUp: 38, keyDown: 40}); // up/down arrows
-player2 = player({name: 'gustaf', keyUp: 65, keyDown: 90}); // a - up, z - down
+player1 = Player({name: 'klas', keyUp: 38, keyDown: 40}); // up/down arrows
+player2 = Player({name: 'gustaf', keyUp: 65, keyDown: 90}); // a - up, z - down
