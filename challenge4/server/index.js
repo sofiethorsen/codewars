b72b2971ee6ad854ddef2514f1491e151756bb45
Sofie Thorsen <sofie.l.thorsen@gmail.com>
2014-02-22 15:29:49 +0100

    Add base for challenge4

---
+++ challenge4/server/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+
+
+var game = require('./game/index.js');
+
+var app = express()
+  , server = require('http').createServer(app)
+  , io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+app.use("/js", express.static(__dirname + '/client/js'));
+app.use("/css", express.static(__dirname + '/client/css'));
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/client/index.html');
+});
+
+io.sockets.on('connection', function (socket) {
+  game.setPlayer(socket);
+});
